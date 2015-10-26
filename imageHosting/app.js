cd20277fb5327e3735a18959a7e050fca70f4e0f
weixing <weixing@jovision.com>
2015-10-26 16:37:40 +0800

    upload file and store in redis

--- imageHosting/app.js
+++ imageHosting/app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use('/static',express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', routes);
 app.use('/users', users);
--- imageHosting/routes/upload.js
+++ imageHosting/routes/upload.js
@@ -4,22 +4,52 @@
 
 
 var express = require('express');
-var router = express.Router();
+var crypto = require('crypto');
+var redis = require('redis');
 
+var router = express.Router();
+var client = redis.createClient(6379,'127.0.0.1', {'return_buffers': true});
+
+var fileSys = require('fs');
+
+client.on('error',function(error){
+    console.log(error);
+});
 
 var multer  = require('multer');//file upload header parser
 var t = multer({dest:'./uploads'});
-
-router.use(function timeLog(req, res, next) {
-    next();
-})
 
 router.get('/', function(req, res, next) {
     res.render('uploadform');
 });
 router.post('/', t.single('photo'),function(req,res,next){
-    console.log(req.file);
-    res.send('upload file');
+    fileSys.readFile(req.file.path, function (err, data) {
+        if (err) throw err;
+        fileSys.unlink(req.file.path);
+        var md5 = crypto.createHash('md5');
+        md5.update(data);
+        var key = md5.digest('hex');
+        client.set(key,data, function(error, r) {
+            res.send(key);
+        });
+    });
 });
+
+router.get('/:key',function(req,res){
+    var key = req.params.key;
+    client.get(key,function(error,data){
+        if(error) throw error;
+        res.writeHead(200,{'Content-Type':'image/png'});
+        res.end(data,'binary');
+    });
+});
+
+router.get('/test/:key',function(req,res){
+    fileSys.readFile('uploads/1.png',function(error,data){
+        res.writeHead(200, {'Content-Type': 'image/png' });
+        res.end(data,'binary');
+    });
+});
+
 
 module.exports = router;
